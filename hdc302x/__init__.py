8390a5512dbb48e8472a24c725e3e7432290e5fa
Paul <78382302+PaulyMooch89@users.noreply.github.com>
2025-06-11 13:59:09 -0400

    Update __init__.py

--- hdc302x/__init__.py
+++ hdc302x/__init__.py
@@ -1,1 +1,19 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import i2c, sensor
+from esphome.const import CONF_ID, CONF_TEMPERATURE, CONF_HUMIDITY
 
+from .sensor import HDC302XComponent, CONFIG_SCHEMA
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield i2c.register_i2c_device(var, config)
+
+    if CONF_TEMPERATURE in config:
+        sens = yield sensor.new_sensor(config[CONF_TEMPERATURE])
+        cg.add(var.set_temperature_sensor(sens))
+
+    if CONF_HUMIDITY in config:
+        sens = yield sensor.new_sensor(config[CONF_HUMIDITY])
+        cg.add(var.set_humidity_sensor(sens))
